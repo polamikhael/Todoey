f889d40c26c08b59c8bc10e77dcff067c4c52d38
Pola Mikhael <polamikhael@polas-mbp.attlocal.net>
2018-09-04 05:24:23 -0500

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Pola Mikhael on 9/3/18.
+//  Copyright © 2018 Pola Mikhael. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
